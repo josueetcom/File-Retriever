772bb6192f70e7d3f3999f2ce81d879aafd62e5f
Sixto Rios Rodriguez <t-sirio@microsoft.com>
2016-08-23 16:53:54 -0700

    fixed android setup

--- setup.sh
+++ setup.sh
@@ -4,9 +4,11 @@
 curl https://dl.google.com/android/android-sdk_r24.4.1-linux.tgz -o android-sdk-linux.tgz
 tar tzf android-sdk-linux.tgz $ANDROID_HOME
 
-android="$ANDROID_HOME/tools/android"
+function install {
+  for package; do
+    $ANDROID_HOME/tools/android update sdk -u -t $package <<< "y"
+  done
+}
 
-android update sdk -u -t android-24 <<< "y"
-android update sdk -u -t build-tools-24.0.1 <<< "y"
-android update sdk -u -t platform-tools <<< "y"
-android update sdk -u -t tools <<< "y"
+install android-24 build-tools-24.0.1 platform-tools tools extra-android-m2repository
+
