7a0a09535b18fa24bbd9591e785f6de47c5b3cce
Sixto Rios Rodriguez <t-sirio@microsoft.com>
2016-08-23 14:30:30 -0700

    added shippable

--- app/src/test/java/me/josueetcom/file_retriever/ExampleUnitTest.java
+++ app/src/test/java/me/josueetcom/file_retriever/ExampleUnitTest.java
@@ -8,8 +8,19 @@ import static org.junit.Assert.*;
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
+
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+        assertEquals(4, 2 + 1);
+    }
+
+    @Test
+    public void subtraction_isCorrect() throws Exception {
+        assertEquals(1, 2 - 1);
+    }
+
+    @Test
+    public void multiplication_isCorrect() throws Exception {
+        assertEquals(4, 2 * 2);
     }
 }
