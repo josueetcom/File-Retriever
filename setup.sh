5eefe1456a9c93b06938873a8523c44d97d3d88f
Sixto Rios Rodriguez <t-sirio@microsoft.com>
2016-08-23 17:20:51 -0700

    Added caching

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-
-# Download Android SDK
-curl https://dl.google.com/android/android-sdk_r24.4.1-linux.tgz -o android-sdk-linux.tgz
-tar xzf android-sdk-linux.tgz
-
-function install {
+function sdk_install {
   for package; do
     $ANDROID_HOME/tools/android update sdk -u -t $package <<< "y"
   done
 }
 
-install android-24 build-tools-24.0.1 platform-tools tools extra-android-m2repository
+if [[ -d $ANDROID_HOME ]]; then
+  # Download Android SDK if it isn't cached
+  curl https://dl.google.com/android/android-sdk_r24.4.1-linux.tgz -o android-sdk-linux.tgz
+  tar xzf android-sdk-linux.tgz
 
+  sdk_install android-24 build-tools-24.0.1 platform-tools tools extra-android-m2repository
+fi
