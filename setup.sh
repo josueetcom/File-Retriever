3200c8ca55a604841637607ac456136209e9d375
Sixto Rios Rodriguez <t-sirio@microsoft.com>
2016-08-23 17:06:05 -0700

    tarring works

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # Download Android SDK
 curl https://dl.google.com/android/android-sdk_r24.4.1-linux.tgz -o android-sdk-linux.tgz
-tar xzf android-sdk-linux.tgz -f $ANDROID_HOME
+tar xzf android-sdk-linux.tgz
 
 function install {
   for package; do
