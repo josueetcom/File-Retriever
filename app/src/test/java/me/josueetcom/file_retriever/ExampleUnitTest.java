14aa76099adb195aaf9a834384f1b732140f1aba
Sixto Rios Rodriguez <t-sirio@microsoft.com>
2016-08-24 10:30:19 -0700

    Make tests pass

--- app/src/test/java/me/josueetcom/file_retriever/ExampleUnitTest.java
+++ app/src/test/java/me/josueetcom/file_retriever/ExampleUnitTest.java
@@ -11,7 +11,7 @@ public class ExampleUnitTest {
 
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 1);
+        assertEquals(4, 2 + 2);
     }
 
     @Test
